894e8e6e63b22ae9fdaa229d1baf7ba0b597fe4d
sanjupaswan <sanjupaswan224@gmail.com>
2026-06-09 18:56:23 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newwer version
+//alert(3 + 3) // we are using node.js not browser so alert will not work
+
+let Name = "sanju"
+let age = 22
+let isApproved = true
+let firstName = undefined
+let selectedColor = null
+
+
+
+// Primitives data types in JavaScript are:
+
+//number =>2 to power 53-1
+// biginint => larger than 2 to power 53-1
+// string => "" sequence of characters
+// boolean => true or false
+// undefined => variable is declared but not assigned a value
+// null => variable is explicitly set to have no value
+
+
+//Objests
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
